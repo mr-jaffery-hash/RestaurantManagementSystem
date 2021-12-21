2f1b1b470aca72a907ad21783f923c377848fe7b
Hussa <Hussa@192.168.56.1>
2021-12-21 16:11:37 +0500

    Check rms.java file. We will implement every insert query like this for every button.

--- src/rms/Rms.java
+++ src/rms/Rms.java
@@ -5,6 +5,11 @@
  */
 package rms;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.DriverManager; 
+import java.sql.SQLException;
+import java.sql.ResultSet;
 /**
  *
  * @author Lenovo
@@ -13,9 +18,23 @@ public class Rms {
 
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SQLException {
+        try {
+            // TODO code application logic here
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String dburl = "jdbc:sqlserver://DESKTOP-E3EOGA4;databaseName=rms;integratedSecurity=true";
+            java.sql.Connection con = DriverManager.getConnection(dburl);
+            java.sql.Statement stat = con.createStatement();
+            String query = "Select * from customer";
+            ResultSet rs= stat.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString(1));
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Rms.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
