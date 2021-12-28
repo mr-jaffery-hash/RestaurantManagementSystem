ee67d85cce03d0c448edbbc222105804f3512ec5
alizeeshan604 <iazeeshan.online@gmail.com>
2021-12-28 15:05:53 +0500

    customer order done

---
+++ src/rms/businessObjectLayer/custorder.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rms.businessObjectLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import rms.DeliveryGuyInterface;
+
+/**
+ *
+ * @author Haier
+ */
+public class custorder {
+
+    public custorder(int orderno, String[] items, int countitems) {
+        this.orderno = orderno;
+        this.items = items;
+        this.countitems = countitems;
+    }
+    public void sendorder(){
+    try{
+     String items[]=this.items;
+       boolean check = false;
+       System.out.print(this.orderno);
+       //Class.forName=(com.mysql.jdbc.Driver);
+       Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/rms","root","1234");
+       Statement st=con.createStatement();
+       for(int i=0;i<this.countitems;i++){
+       String sql="INSERT INTO `rms`.`custorder`" +
+"(`idcustorder`," +
+"`item`)" +
+"VALUES('"+this.orderno+"','"+items[0]+"')";
+       st.executeUpdate(sql);}
+    }   
+       catch(Exception e){
+       System.out.println(e.getMessage());
+       }
+    
+    }
+    
+    public int orderno;
+    public String items[];
+    public int countitems;
+    
+    
+}
